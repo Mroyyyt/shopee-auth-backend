5c70a4b5d8b72a79e390d8c3e9ff826acee78efd
Mroyyyt <86856262+Mroyyyt@users.noreply.github.com>
2025-05-07 11:05:53 +0700

    Delete api/callback.js

--- api/callback.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const { code, shop_id } = req.query;
-
-  // Simpan code dan shop_id, atau lakukan proses lain
-  res.status(200).json({ message: "Callback diterima", code, shop_id });
-}
