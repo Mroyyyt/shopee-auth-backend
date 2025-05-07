ec932446dba654432df214dbfce1848a4186c277
Mroyyyt <86856262+Mroyyyt@users.noreply.github.com>
2025-05-07 11:55:56 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Shopee Auth Backend</h1>
+      <p>Gunakan endpoint <code>/api/generate</code> untuk memulai auth.</p>
+    </div>
+  );
+}
