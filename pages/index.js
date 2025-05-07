18bf8b3916d5d0b8bd85aa59a5acf8392205fc06
Mroyyyt <86856262+Mroyyyt@users.noreply.github.com>
2025-05-07 12:45:36 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
 export default function handler(req, res) {
-  const { code, shop_id } = req.query;
-
-  if (code && shop_id) {
-    res.status(200).json({ message: "Callback diterima", code, shop_id });
-  } else {
-    res.status(200).send("Shopee Auth Redirect: Tidak ada kode dikirim.");
-  }
+  res.status(200).json({ name: 'shopee-auth-backend' })
 }
