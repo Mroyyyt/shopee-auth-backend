a2c741936c7eeff8d48729b7ac05cb3095b55ba4
Mroyyyt <86856262+Mroyyyt@users.noreply.github.com>
2025-05-07 12:19:57 +0700

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Shopee Auth Backend</h1>
-      <p>Gunakan endpoint <code>/api/generate</code> untuk memulai auth.</p>
-    </div>
-  );
-}
