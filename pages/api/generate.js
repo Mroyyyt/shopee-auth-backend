f93e69d62b52d3898b10814fb499a8c7dc30fb99
Mroyyyt <86856262+Mroyyyt@users.noreply.github.com>
2025-05-07 12:32:36 +0700

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -3,7 +3,7 @@ const crypto = require("crypto");
 export default function handler(req, res) {
   const partner_id = process.env.PARTNER_ID;
   const partner_key = process.env.PARTNER_KEY;
-  const redirect = process.env.REDIRECT_URL;
+  const redirect = process.env.REDIRECT_URL; // Harus sama dengan yang di Shopee
 
   const path = '/api/v2/shop/auth_partner';
   const timestamp = Math.floor(Date.now() / 1000);
@@ -11,6 +11,6 @@ export default function handler(req, res) {
   const sign = crypto.createHmac('sha256', partner_key).update(baseString).digest('hex');
 
   const url = `https://partner.shopeemobile.com${path}?partner_id=${partner_id}&timestamp=${timestamp}&sign=${sign}&redirect=${redirect}`;
-
+  
   res.redirect(url);
 }
